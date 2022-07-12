b3d3ddc890ae1c1d6843676341b98ce7a89ea804
Garcia-Maturana <juanjesusgarciamaturana@gmail.com>
2022-07-11 21:00:45 -0500

    Primer version del Menu

---
+++ src/App_Menu_Clases/ConexionBD.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package App_Menu_Clases;
+/**
+ *
+ * @author numbe
+ */
+public class ConexionBD {
+    
+    
+}
